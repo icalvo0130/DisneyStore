70f6abce7ee36cc3cbf4501536ed06049b16e2f7
marianaparrac9 <marianapch29@gmail.com>
2024-10-26 12:04:34 -0500

    chore:feat:utils folder created for code splitting (functions)

---
+++ utils/utils.js
@@ -0,0 +1,30 @@
+    //export const for obtain the users that are registered
+    export const obtenerUsuarios = () => {
+        const usuarios = localStorage.getItem('user');
+        if (!usuarios){
+            return [];
+        }
+        return JSON.parse(usuarios);
+    };
+    
+    
+    //export const for obtain the active user
+        export const obtenerUsuarioActivo = () => {
+        const usuarioActivo = parseInt(localStorage.getItem('user-active'));
+        const usuarios = obtenerUsuarios();
+        // console.log(usuarioActivo); 
+        // console.log(usuarios); 
+    
+        for (const usuario of usuarios){
+            if (usuario.id === usuarioActivo){
+                // console.log(usuario);
+                return usuario;
+            }
+        }
+        return null;
+    };
+    export const logout = () => {
+        localStorage.removeItem(usuarioActivoKey);    
+    };
+    
+    
